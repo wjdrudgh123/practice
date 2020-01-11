8b8e43ef047ddf5c2aa69cb9c7368c91590cbf60
Jung-Jaeho <wjdrudgh123@naver.com>
2020-01-11 14:05:16 +0900

    practice

---
+++ js/action.js
@@ -0,0 +1,19 @@
+const content = document.querySelector(".js-content"),
+  like = content.querySelector(".js-like"),
+  image = content.querySelector(".js-up-image");
+
+function endEvent() {
+  image.classList.remove("on");
+  image.classList.add("off");
+}
+function clickLikes() {
+  image.classList.remove("off");
+  image.classList.add("on");
+  setTimeout(endEvent, 3000);
+}
+
+function init() {
+  like.addEventListener("click", clickLikes);
+}
+
+init();
